b18d8171f95e6ed54f1d1359c6be71835f4dcce5
Phillip Jones <phillipemersonjones@gmail.com>
2016-07-16 14:53:17 -0500

    added a arrow to the bottom of the header section

---
+++ app/assets/javascripts/header.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function(){
+  var header_btn = document.getElementById('header-btn');
+  header_btn.addEventListener('click', function (){
+    jump('#contact-form');
+  })
+})
